4159f54f7f5780b4f1103dd45170fd61177ab97e
Edmund <eyu1@ualberta.ca>
2021-10-28 21:18:35 -0600

    Remove days array, make axios call to api/days to get days data

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,26 +1,8 @@
-import React, { useState } from "react";
-
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import "components/Application.scss";
 import DayList from "./DayList";
 import Appointment from "./Appointment";
-
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
 
 const appointments = [
   {
@@ -62,7 +44,18 @@ const appointments = [
 ];
 
 export default function Application(props) {
-  const [day, setDay] = useState("Monday");
+  const [days, setDays] = useState([]);
+  useEffect(() => {
+    axios
+      .get("http://localhost:8001/api/days")
+      .then((response) => {
+        setDays(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, [days]);
+
   return (
     <main className="layout">
       <section className="sidebar">
@@ -73,7 +66,7 @@ export default function Application(props) {
         />
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu">
-          <DayList days={days} value={day} onChange={setDay} />
+          <DayList days={days} value={days} onChange={setDays} />
         </nav>
         <img
           className="sidebar__lhl sidebar--centered"
